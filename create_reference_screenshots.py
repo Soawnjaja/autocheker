187f4e5ab85c047a916f6650407a0c4458ea0ece
Soawnjaja <basstraper@gmail.com>
2024-06-18 23:07:35 +0300

    test ssim

--- create_reference_screenshots.py
+++ create_reference_screenshots.py
@@ -1,5 +1,8 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Настройка опций Chrome
 options = webdriver.ChromeOptions()
@@ -10,16 +13,13 @@ driver = webdriver.Chrome(options=options)
 
 # Путь к главной папке приложения
 base_path = os.path.dirname(os.path.abspath(__file__))
-reference_path = os.path.join(base_path, 'reference')
-os.makedirs(reference_path, exist_ok=True)
+screenshots_path = os.path.join(base_path, 'screenshots')
+os.makedirs(screenshots_path, exist_ok=True)
 
 # Список URL сайтов для проверки
 urls = [
-    "https://artgroup76.ru",
     "https://print-one.ru",
-    "https://interstone.su",
-    "https://peachdesign.ru"
-    # Добавьте все необходимые сайты
+    "https://interstone.su"
 ]
 
 # Размеры устройств
@@ -33,16 +33,13 @@ devices = {
 def create_screenshot(url, device_name, device_size):
     driver.set_window_size(*device_size)
     driver.get(url)
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
     domain_name = url.replace("https://", "").replace("http://", "").replace("/", "_").replace(":", "_").replace(".", "_")
-    screenshot_path = os.path.join(reference_path, f'{device_name}_{domain_name}.png')
-    print(f"Путь для сохранения скриншота: {screenshot_path}")
-    try:
-        driver.save_screenshot(screenshot_path)
-        print(f"Скриншот для {url} на устройстве {device_name} сохранен.")
-    except Exception as e:
-        print(f"Ошибка при сохранении файла: {e}")
+    screenshot_path = os.path.join(screenshots_path, f'{device_name}_{domain_name}_home.png')
+    driver.save_screenshot(screenshot_path)
+    print(f"Скриншот для {url} на устройстве {device_name} сохранен.")
 
-# Создание эталонных скриншотов
+# Создание скриншотов
 for url in urls:
     for device_name, device_size in devices.items():
         create_screenshot(url, device_name, device_size)
--- script.py
+++ script.py
@@ -1,6 +1,18 @@
 import os
+
 from selenium import webdriver
-from PIL import Image, ImageChops
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from skimage.metrics import structural_similarity as ssim
+import cv2
+
+# Настройка опций Chrome
+options = webdriver.ChromeOptions()
+options.headless = True 
+
+# Создание экземпляра драйвера
+driver = webdriver.Chrome(options=options)
 
 # Путь к главной папке приложения
 base_path = os.path.dirname(os.path.abspath(__file__))
@@ -9,42 +21,44 @@ screenshots_path = os.path.join(base_path, 'screenshots')
 os.makedirs(reference_path, exist_ok=True)
 os.makedirs(screenshots_path, exist_ok=True)
 
-# Настройка WebDriver
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(options=options)
-
 # Список URL сайтов для проверки
-urls = ["https://artgroup76.ru", "https://print-one.ru", "https://interstone.su", "https://print-one.ru/"]
+urls = [
+    "https://print-one.ru",
+    "https://interstone.su"
+]
 
 # Размеры устройств
-devices = {"desktop": (1920, 1080), "tablet": (768, 1024), "mobile": (375, 667)}
+devices = {
+    "desktop": (1920, 1080),
+    "tablet": (768, 1024),
+    "mobile": (375, 667),
+}
 
-def create_screenshot(url, device_name, device_size):
+# Функция для создания и сравнения скриншотов
+def create_and_compare_screenshot(url, device_name, device_size):
     driver.set_window_size(*device_size)
     driver.get(url)
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
     domain_name = url.replace("https://", "").replace("http://", "").replace("/", "_").replace(":", "_").replace(".", "_")
-    screenshot_path = os.path.join(screenshots_path, f'{device_name}_{domain_name}.png')
-    driver.save_screenshot(screenshot_path)
-    return screenshot_path
+    screenshot_filename = f'{device_name}_{domain_name}_home.png'
+    screenshot_path = os.path.join(screenshots_path, screenshot_filename)
+    reference_screenshot_path = os.path.join(reference_path, screenshot_filename)
 
-def compare_screenshots(reference_path, test_path):
-    try:
-        image_one = Image.open(reference_path)
-        image_two = Image.open(test_path)
-        diff = ImageChops.difference(image_one, image_two)
-        return not diff.getbbox()
-    except FileNotFoundError:
-        print(f"Файл не найден: {reference_path}")
-        return False
- 
-# Проверка сайтов
+    driver.save_screenshot(screenshot_path)
+    print(f"Скриншот для {url} на устройстве {device_name} сохранен.")
+
+    # Сравнение скриншотов
+    if os.path.exists(reference_screenshot_path):
+        original = cv2.imread(reference_screenshot_path, cv2.IMREAD_GRAYSCALE)
+        new_shot = cv2.imread(screenshot_path, cv2.IMREAD_GRAYSCALE)
+        score, _ = ssim(original, new_shot, full=True)
+        print(f"SSIM score for {url} on {device_name}: {score}")
+    else:
+        print(f"No reference screenshot found for {url} on {device_name}. Please check the reference directory.")
+
+# Создание и сравнение скриншотов
 for url in urls:
     for device_name, device_size in devices.items():
-        temp_screenshot = create_screenshot(url, device_name, device_size)
-        ref_screenshot_path = os.path.join(reference_path, f'{device_name}_{url.replace("https://", "").replace("http://", "").replace("/", "_").replace(":", "_").replace(".", "_")}.png')
-        if compare_screenshots(ref_screenshot_path, temp_screenshot):
-            print(f"Сайт {url} на устройстве {device_name} соответствует эталону.")
-        else:
-            print(f"Обнаружены изменения на сайте {url} на устройстве {device_name}.")
+        create_and_compare_screenshot(url, device_name, device_size)
 
 driver.quit()
